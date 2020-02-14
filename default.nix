a01e97f02ba1282a4b5c8fb6e7443f66e62dc756
Sean Hickman <sean@grumply.com>
2020-02-13 23:32:22 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure, pure-cond, pure-json, pure-lifted, pure-prop, stdenv }:
 mkDerivation {
   pname = "pure-intersection";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure pure-cond pure-json pure-lifted pure-prop ];
   homepage = "github.com/grumply/pure-intersection";
